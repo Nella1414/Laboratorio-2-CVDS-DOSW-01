1564c82a2537a9ca3d74a9d253f767ac147d27aa
Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>
2025-08-21 14:02:20 -0500

    feat: add maven files

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Laboratorio_2_CVDS_DOSW_01/Laboratorio2CvdsDosw01Application.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Laboratorio_2_CVDS_DOSW_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio2CvdsDosw01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio2CvdsDosw01Application.class, args);
+	}
+
+}
