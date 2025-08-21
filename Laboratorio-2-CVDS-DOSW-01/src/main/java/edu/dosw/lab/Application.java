01d2273309b264c773ee15b4770bccc80d0a86e8
Nella1414 <marianella.polo-p@mail.escuelaing.edu.co>
2025-08-21 14:15:41 -0500

    feat(laboratorio-2): add initial Java application and restructure docs

---
+++ Laboratorio-2-CVDS-DOSW-01/src/main/java/edu/dosw/lab/Application.java
@@ -0,0 +1,7 @@
+package edu.dosw.lab
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Maveen configurado y correindo correctamente!");
+    }
+}
